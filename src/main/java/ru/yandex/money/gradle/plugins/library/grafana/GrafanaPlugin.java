8eaf36d73eaa67cd284a49cc1bcc51fe33c78233
Vasily Sozykin <vsozykin@yamoney.ru>
2018-07-16 23:31:55 +0300

    убрал зависимость от git

--- src/main/java/ru/yandex/money/gradle/plugins/library/grafana/GrafanaPlugin.java
+++ src/main/java/ru/yandex/money/gradle/plugins/library/grafana/GrafanaPlugin.java
@@ -4,7 +4,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import ru.yandex.money.gradle.plugins.library.grafana.settings.GrafanaConnectionExtension;
 import ru.yandex.money.gradle.plugins.library.grafana.settings.GrafanaConnectionSettings;
-import ru.yandex.money.gradle.plugins.library.helpers.GitRepositoryProperties;
 
 /**
  * Плагин для работы с Grafana
@@ -21,9 +20,7 @@ public class GrafanaPlugin implements Plugin<Project> {
      */
     @Override
     public void apply(Project target) {
-        GitRepositoryProperties gitProperties = new GitRepositoryProperties(target.getProjectDir().getAbsolutePath());
-
-        UpsertGrafanaDashboardsTask upsertGrafanaDashboardsTask = upsertGrafanaDashboardTask(target, gitProperties);
+        UpsertGrafanaDashboardsTask upsertGrafanaDashboardsTask = upsertGrafanaDashboardTask(target);
 
         GrafanaConnectionExtension grafanaConnection =
                 target.getExtensions().create(GRAFANA_EXTENSION_NAME, GrafanaConnectionExtension.class);
@@ -32,10 +29,9 @@ public class GrafanaPlugin implements Plugin<Project> {
                 upsertGrafanaDashboardsTask.setGrafanaConnectionSettings(newGrafanaConnectionSettings(grafanaConnection)));
     }
 
-    private static UpsertGrafanaDashboardsTask upsertGrafanaDashboardTask(Project target, GitRepositoryProperties gitRepositoryProperties) {
+    private static UpsertGrafanaDashboardsTask upsertGrafanaDashboardTask(Project target) {
         UpsertGrafanaDashboardsTask task = target.getTasks().create(UpsertGrafanaDashboardsTask.TASK_NAME,
                 UpsertGrafanaDashboardsTask.class);
-        task.onlyIf(element -> gitRepositoryProperties.isDevBranch());
         task.setGroup("other");
         task.setDescription("Upsert Grafana dashboard");
         return task;
